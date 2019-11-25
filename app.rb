ff696948e7dcbba731394f425443a1d76b9da80e
Quentin Crida <qcrida@gmail.com>
2019-11-25 22:15:48 +0000

    stuck at this point

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/game')
 also_reload('./models/*')
 
-get '/check_hand/:hand1/:hand2' do
+get '/check-hand/:hand1/:hand2' do
   hand1 = params['hand1']
   hand2 = params['hand2']
   @result = Game.check_hand(hand1, hand2)
