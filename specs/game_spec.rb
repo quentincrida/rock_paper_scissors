23caba41f2753fde0a9f735cecd370a1b940a895
Quentin Crida <qcrida@gmail.com>
2019-11-26 09:54:53 +0000

    spec working

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -4,6 +4,6 @@ require_relative('../models/game')
 class TestGame < MiniTest:: Test
 
   def test_check_hand
-    assert_equal ('hand1 wins', Game.check_hand('scissors', 'paper'))
+    assert_equal('hand1 wins', Game.check_hand('scissors', 'paper'))
   end
 end
